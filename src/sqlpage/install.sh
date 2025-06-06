d733a73470664ff00d490a24d15bb1ed5ad98eb0
Amy W <49384+keisisqrl@users.noreply.github.com>
2025-06-06 15:40:38 -0700

    add sqlpage feature

---
+++ src/sqlpage/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'sqlpage'"
+
+curl -L curl -L https://github.com/sqlpage/SQLPage/releases/download/v0.35.2/sqlpage-linux.tgz | tar -zxf - sqlpage.bin
+mv sqlpage.bin /usr/local/bin/sqlpage
+chmod 755 /usr/local/bin/sqlpage
